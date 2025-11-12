57853f549d15df1f0db315e33b60b583c9811735
francisco.3.fernandes@gmail.com <francisco.3.fernandes@gmail.com>
2025-11-12 10:39:02 +0000

    C# App Start To Finish Lesson 06 - Class Library Creation

---
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class MatchupEntryModel
+    {
+        /// <summary>
+        /// Representa uma equipa no jogo
+        /// </summary>
+        public TeamModel TeamCompeting { get; set; }
+
+        /// <summary>
+        /// Representa a pontuação de uma determinada equipa
+        /// </summary>
+        public double Score { get; set; }
+
+        /// <summary>
+        /// Representa o confronto de onde esta equipa saiu vencedora. 
+        /// </summary>
+        public MatchupModel ParentMatchup { get; set; }
+    }
+}
