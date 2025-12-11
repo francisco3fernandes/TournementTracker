40a84b519087025c359b0224cf39f1c528235baa
francisco.3.fernandes@gmail.com <francisco.3.fernandes@gmail.com>
2025-12-11 09:03:33 +0000

    Error Handling

--- TrackerUI/TournamentViewerForm.cs
+++ TrackerUI/TournamentViewerForm.cs
@@ -150,9 +150,45 @@ namespace TrackerUI
             LoadMatchups((int)roundropDown.SelectedItem);
 
         }
+        private string ValidateData()
+        {
+            string output = "";
+
+            double teamOneScore = 0;
+            double teamTwoScore = 0;
+
+            bool scoreOneValid = double.TryParse(teamOneScoreValue.Text, out teamOneScore);
+            bool scoreTwoValid = double.TryParse(teamTwoScoreValue.Text, out teamTwoScore);
+
+            if (!scoreOneValid)
+            {
+                output = "The Score One value is not a valid number.";
+            }
+            else if (!scoreTwoValid)
+            {
+                output = "The Score Two value is not a valid number.";
+            }
+            else if (teamOneScore == 0 && teamTwoScore == 0)
+            {
+                output = "You did not enter a score for either team";
+            }
+            else if (teamOneScore == teamTwoScore)
+            {
+                output = "We do not allow ties in this application";
+            }
+
+            return output;
+        }
 
         private void scoreButton_Click(object sender, EventArgs e)
         {
+            string errorMessage = ValidateData();
+            if (errorMessage.Length > 0)
+            {
+                MessageBox.Show($"Input Error: {errorMessage}");
+                return;
+            }
+
             MatchupModel m = (MatchupModel)matchupListBox.SelectedItem;
             double teamOneScore = 0;
             double teamTwoScore = 0;
@@ -175,11 +211,6 @@ namespace TrackerUI
                             return;
                         }
                     }
-                    else
-                    {
-                        teamOneName.Text = "Not Yet Set";
-                        teamOneScoreValue.Text = "";
-                    }
                 }
                 if (i == 1)
                 {
@@ -196,18 +227,20 @@ namespace TrackerUI
                                 MessageBox.Show("Please enter a valid score for team 2");
                                 return;
                             }
-
-                            teamTwoName.Text = m.Entries[0].TeamCompeting.TeamName;
-                            teamTwoScoreValue.Text = m.Entries[1].Score.ToString();
-
-                       
                     }
-              
-                }
-         
-            }
-            TournamentLogic.UpdateTournamentResults(tournament);
 
+                }
+
+            }
+            try
+            {
+                TournamentLogic.UpdateTournamentResults(tournament);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"The application had the following error: { ex.Message }");
+                return;
+            }
             LoadMatchups((int)roundropDown.SelectedItem);
         }
     }
